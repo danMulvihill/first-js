b38a32e614596111c620c38167803716b5460002
Dan Mulvihill <dmulvihill3@gmail.com>
2018-01-25 19:30:37 -0500

    Add some vars and a function

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log("Welcome!");
+
+
+//a function that updates the DOM
+function change(){
+    //function body:
+    document.getElementById("foo").innerText = name
+    return 600;
+}
+
+var name = "Lucille";
+var numberOfWidgets = 100;
+var isCodingHard =true;
+
