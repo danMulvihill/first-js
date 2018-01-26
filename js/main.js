0bc15891bb19975087cd503a34d12e0df6324408
Dan Mulvihill <dmulvihill3@gmail.com>
2018-01-25 20:59:21 -0500

    finished for the day; added a couple for functions

--- js/main.js
+++ js/main.js
@@ -8,11 +8,7 @@ function change(){
     
 }
 
-function change2(){
-    //function body:
-    document.getElementById("foo").innerText = greets[2];
-    
-}
+
 
 var name = "Lucille";
 var numberOfWidgets = 100;
@@ -26,3 +22,44 @@ var greets = ["Hello!", "Howdy", "Howdy Y'all!", "Greetings", "Hi.", "Yo!"];
 
 console.log(greets[5])
 
+var meat = ["pork", "beef", "chicken", "duck" ];
+var fruit = ["tomatoes", "squash", "pineapple"];
+var food = [meat, fruit];
+
+console.log(food[1][2]);
+
+var user = {
+    'firstName': "Lucille",
+    age: 40,
+    "lastName": "Bluth",
+    hasHair: true,
+    "has-feet": true
+};
+
+
+var num = 3.5;
+var num2 = 5.6;
+var sum = num + num2;
+
+function add(){
+    //function body:
+    document.getElementById("foo").innerHTML = sum;
+    
+}
+
+var string1 = "Hello ";
+var string2 = "JavaScript!"
+var comboString = string1+string2
+
+function combineStr(){
+    document.getElementById("foo").innerHTML = comboString;
+}
+
+var me ={
+    firstName: "Dan",
+    lastName: "Mulivhill",
+    fingerCount: 10,
+    interests: ["computer stuff", "CSS" , "JS", "sleeping"]
+}
+
+console.log(me.interests[3]);
