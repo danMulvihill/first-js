2b4dff1a2a0c5a09d4904eb8332e702e99589b9e
Dan Mulvihill <dmulvihill3@gmail.com>
2018-01-25 20:03:54 -0500

    Made lots of stuff

--- js/main.js
+++ js/main.js
@@ -4,11 +4,25 @@ console.log("Welcome!");
 //a function that updates the DOM
 function change(){
     //function body:
-    document.getElementById("foo").innerText = name
-    return 600;
+    document.getElementById("foo").innerText = greets[3];
+    
+}
+
+function change2(){
+    //function body:
+    document.getElementById("foo").innerText = greets[2];
+    
 }
 
 var name = "Lucille";
 var numberOfWidgets = 100;
 var isCodingHard =true;
+
+var characters = [50, true, "Lucille", "Michael Bluth", "Tobias", 10 , 2];
+
+console.log(characters[0]);
+
+var greets = ["Hello!", "Howdy", "Howdy Y'all!", "Greetings", "Hi.", "Yo!"];
+
+console.log(greets[5])
 
